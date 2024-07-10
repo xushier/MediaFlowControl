e196ff355f159ebd8d8e0091ce8eac4dbdbd87a6
小迪同学 <31540107+xushier@users.noreply.github.com>
2024-07-10 08:36:17 +0800

    Update MediaAM.py

--- tools/MediaAM.py
+++ tools/MediaAM.py
@@ -299,7 +299,7 @@ class FileInfo:
                             upload_after_time = self.control_dict[cd2_cloud_file_path]['upload_time']
                             process_count = self.control_dict[cd2_cloud_file_path]['process_count']
                             new_control_dict[cd2_cloud_file_path] = {'process_count': process_count, 'upload_time': upload_after_time}
-                            self.logger.put(f"初始化程序：该文件处理过 {process_count} 次，上传累计需等待 {upload_time} 小时，等待时间过久，启动首传：{file_name}")
+                            self.logger.put(f"初始化程序：该文件处理过 {process_count} 次，上传累计需等待 {upload_after_time} 小时，等待时间过久，启动首传：{file_name}")
                             continue
 
 
